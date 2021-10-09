ff5dd0660a6f1969ea63d24733a8db5616a6ab2f
GIT-DS <darrickshin@gmail.com>
2021-10-09 15:23:25 -0700

    Set up Db

---
+++ config/key.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://dbUser:g0uhIMCGpTHRTBww@cluster0.ab5ct.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+              //Make sure this is your own unique string
+  }
