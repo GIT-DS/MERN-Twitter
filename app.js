686bd5cd0fcdd9935911d186792b6db5f1f2297e
GIT-DS <darrickshin@gmail.com>
2021-10-09 18:27:56 -0700

    Set up validations

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("I'm Working Again!"));
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
 const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
--- config/key.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://dbUser:g0uhIMCGpTHRTBww@cluster0.ab5ct.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-              //Make sure this is your own unique string
-  }
